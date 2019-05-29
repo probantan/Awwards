3c21512f186154d10acbb7186f410f5fd794ec3c
protus <protus.bantan@gmail.com>
2019-05-29 18:00:24 +0300

    Initial commit

--- awad/models.py
+++ awad/models.py
@@ -32,10 +32,40 @@ class Project(models.Model):
     image=models.ImageField(upload_to='projects/', blank=True)
     detailed_description=models.TextField()
     project_url = models.CharField(max_length =50)
-    deployed_link=models.CharField(max_length=40)
     category=models.ForeignKey(Category,related_name='category',null=True, blank=True, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, null = True,related_name='project')
+    # profile = models.ForeignKey(Profile, null = True,related_name='project')
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
     user= models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-pk']
+
+    def save_project(self):
+        self.save()
+    
+    @classmethod
+    def get_project(cls, profile):
+        project = Project.objects.filter(Profile__pk = profile)
+        return project
+    
+    @classmethod
+    def get_all_projects(cls):
+        project = Project.objects.all()
+        return project
+
+    @classmethod
+    def search_by_profile(cls,search_term):
+        projo = cls.objects.filter(profile__name__icontains=search_term)
+        return projo
+
+    @classmethod
+    def get_profile_projects(cls, profile):
+        project = Project.objects.filter(profile__pk = profile)
+        return project
+
+    @classmethod
+    def find_project_id(cls, id):
+        identity = Project.objects.get(pk=id)
+        return identity
 
 
